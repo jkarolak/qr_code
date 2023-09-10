5911ac170c9b75a2de03a58f1ac83da30b0a38fa
Jakub Karolak <jakub.karolak90@gmail.com>
2023-09-10 12:47:14 +0200

    First commit

---
+++ menu/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Skladnik, PozycjaMenu
+
+admin.site.register(Skladnik)
+admin.site.register(PozycjaMenu)
---
+++ menu/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Skladnik(models.Model):
+    nazwa = models.CharField(max_length=20)
+    #ilosc = models.DecimalField(decimal_places=2, default=1,max_digits=3)
+    #jednostka_miary = models.CharField(choices=[("SZT","szt."),("KG","kg"),("G","g"),("ML","ml.")], default="SZT.",max_length=40)
+
+class PozycjaMenu(models.Model):
+    nazwa = models.CharField(max_length=40)
+    data_utworzenia = models.DateTimeField(auto_now_add=True)
+    data_edytowania = models.DateTimeField(auto_now=True)
+    cena = models.DecimalField(default=0, decimal_places=2,max_digits=3)
+    skladniki = models.ManyToManyField(Skladnik)
+
+
+
