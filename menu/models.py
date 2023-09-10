abd994e0d276a172253d5c36710dfbf91d53ae8f
Jakub Karolak <jakub.karolak90@gmail.com>
2023-09-10 15:07:26 +0200

    testowy commit

--- menu/models.py
+++ menu/models.py
@@ -4,7 +4,7 @@ class Skladnik(models.Model):
     nazwa = models.CharField(max_length=20)
     #ilosc = models.DecimalField(decimal_places=2, default=1,max_digits=3)
     #jednostka_miary = models.CharField(choices=[("SZT","szt."),("KG","kg"),("G","g"),("ML","ml.")], default="SZT.",max_length=40)
-    #test zmian = 
+    #test zmian = tess 
 class PozycjaMenu(models.Model):
     nazwa = models.CharField(max_length=40)
     data_utworzenia = models.DateTimeField(auto_now_add=True)
